f5778958cc473ec4c71e2d5e2d53b3945ffc2b02
nathan <nathan.praties@gmail.com>
2020-03-20 08:02:56 +0000

    route + stage model

---
+++ src/main/java/com/nateprat/model/Route.scala
@@ -0,0 +1,9 @@
+package com.nateprat.model
+
+class Route(name:String, stages:List[Stage]) {
+
+  val name_ = name
+  val stages_ = stages
+
+
+}
---
+++ src/main/java/com/nateprat/model/Stage.scala
@@ -0,0 +1,9 @@
+package com.nateprat.model
+
+class Stage(id:Int, name:String, distanceKm:Float) {
+
+  val id_ = id
+  val name_ = name
+  val distanceKm_ = distanceKm
+
+}
