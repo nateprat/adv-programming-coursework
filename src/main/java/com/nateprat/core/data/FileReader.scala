4243757f73df32570d8f341d6f544fd25fd83571
nathan <nathan.praties@gmail.com>
2020-03-20 08:56:12 +0000

    filereader

---
+++ src/main/java/com/nateprat/core/data/FileReader.scala
@@ -0,0 +1,25 @@
+package com.nateprat.core.data
+
+import com.nateprat.core.pattern.Control
+
+import scala.io.Source
+
+class FileReader {
+
+  def readFile(filePath:String): List[String] = {
+    Control.using(Source.fromResource(filePath)) {
+      source => {
+        source.getLines().toList
+      }
+    }
+  }
+
+  def readResource(filePath:String): List[String] = {
+    Control.using(Source.fromResource(filePath)) {
+      source => {
+        source.getLines().toList
+      }
+    }
+  }
+
+}
