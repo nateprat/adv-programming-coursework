ebf8f9bdb761631472b9b9e388f939050a29c50f
nathan <nathan.praties@gmail.com>
2020-03-26 13:40:10 +0000

    stageextractor test

---
+++ src/test/java/com/nateprat/core/extractor/StageExtractorTest.scala
@@ -0,0 +1,23 @@
+package com.nateprat.core.extractor
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class StageExtractorTest extends AnyFunSuite {
+
+  val underTest = new StageExtractor
+
+  test("extractTest") {
+    val stageString = "1:City Chambers:0.75f"
+    val stage = underTest.extract(stageString)
+    assert(stage.id_ == 1)
+    assert(stage.endPoint_.equals("City Chambers") )
+    assert(stage.distanceKm_ == 0.75f)
+  }
+
+  test("extractFailureTest") {
+    val stageString = "1:City Chambers0.75f"
+    val stage = underTest.extract(stageString)
+    assert(stage == null)
+  }
+
+}
