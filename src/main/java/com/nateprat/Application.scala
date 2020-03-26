da6f6c2c0827d89fc5ab3b0e31af86456baa1644
nathan <nathan.praties@gmail.com>
2020-03-26 13:47:03 +0000

    application forefront

--- src/main/java/com/nateprat/Application.scala
+++ src/main/java/com/nateprat/Application.scala
@@ -1,7 +1,26 @@
 package com.nateprat
 
+import com.nateprat.core.data.FileReader
+import com.nateprat.core.extractor.RouteExtractor
+import com.nateprat.core.transformer.DefaultTransformer
+import com.nateprat.model.{Route, Stage}
+
+import scala.collection.mutable.ListBuffer
+
 object Application extends App {
 
+  val csvReader = new FileReader
 
+  val dataList = csvReader.readResource("data.txt")
+
+  val rawDataTransformer = new DefaultTransformer
+
+
+  val transformer = new DefaultTransformer
+  val routeExtractor = new RouteExtractor
+
+  val routeList = rawDataTransformer.transform[String, Route](dataList, routeExtractor.extract)
+
+  println(routeList)
 
 }
